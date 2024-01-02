9577f45851e84808b3d71c54c33db32209f5ad8d
Irfan Hanif <14.irfanh@gmail.com>
2024-01-02 11:51:16 +0700

    Updated the code

---
+++ skill-test.php
@@ -0,0 +1,28 @@
+<?php
+$data = readline("Masukkan data: ");
+
+preg_match('/(?P<name>.*) (?P<age>\d+)(?: thn| th| tahun| THN| TH| TAHUN)? (?P<city>.*)/', $data, $matches);
+
+$name = strtoupper($matches['name']);
+$age = $matches['age'];
+$city = strtoupper($matches['city']);
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "arkatama_db";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// $createdAt = date("Y-m-d H:i:s");
+
+$sql = "INSERT INTO identity (name, age, city)
+VALUES ('$name', '$age', '$city')";
+
+if (!$conn->query($sql) === TRUE) {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
